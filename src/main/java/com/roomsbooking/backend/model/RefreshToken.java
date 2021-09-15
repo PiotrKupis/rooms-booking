e7446987db947b342270430a8811913b954ea525
Piotr Kupis <piotr.kupis42@gmail.com>
2021-09-15 11:58:40 +0200

    ET-8 add refresh token table

---
+++ src/main/java/com/roomsbooking/backend/model/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.roomsbooking.backend.model;
+
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Class responsible for keeping information about refresh token.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String token;
+
+    @NotNull
+    @Column(name = "creation_date", nullable = false)
+    private Instant createDate;
+}
