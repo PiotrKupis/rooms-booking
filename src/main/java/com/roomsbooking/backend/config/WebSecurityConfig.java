0aaefe76e880707ef3a69a1add20b4c877b1f9cf
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-02 13:16:41 +0200

    ET-16 Configure CORS

--- src/main/java/com/roomsbooking/backend/config/WebSecurityConfig.java
+++ src/main/java/com/roomsbooking/backend/config/WebSecurityConfig.java
@@ -17,9 +17,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @AllArgsConstructor
 @Configuration
@@ -83,23 +80,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
-    /**
-     * Method responsible for registering bean responsible for cors configuration.
-     *
-     * @return bean of type {@link CorsFilter}
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source =
-            new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 }
