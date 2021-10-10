edc104f72411f18a30e214d20ad20beae4c2d442
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-10 18:13:51 +0200

    ET-4 add enum of resort type

---
+++ src/main/java/com/roomsbooking/backend/enums/ResortType.java
@@ -0,0 +1,13 @@
+package com.roomsbooking.backend.enums;
+
+/**
+ * Enum that represents type of resort.
+ */
+public enum ResortType {
+    APARTMENT,
+    HOTEL,
+    MOTEL,
+    HOSTEL,
+    PRIVATE_ACCOMMODATION,
+    HOLIDAY_RESORT;
+}
