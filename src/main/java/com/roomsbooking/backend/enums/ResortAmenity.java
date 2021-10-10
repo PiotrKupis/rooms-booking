fb3b7bd8b968dd39187973249ad8c3a0ef17c49d
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-10 18:15:41 +0200

    ET-4 add enum of resort's amenity

---
+++ src/main/java/com/roomsbooking/backend/enums/ResortAmenity.java
@@ -0,0 +1,17 @@
+package com.roomsbooking.backend.enums;
+
+/**
+ * Enum that represents amenity of resort.
+ */
+public enum ResortAmenity {
+    BAR,
+    SAUNA,
+    GARDEN,
+    TERRACE,
+    JACUZZI,
+    HEATING,
+    FREE_WIFI,
+    AIR_CONDITIONING,
+    SWIMMING_POOL,
+    PARKING;
+}
