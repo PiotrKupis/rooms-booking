ab51bb93b0a9af3531c47245b71225efd7ff9220
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-22 11:32:34 +0200

    add room amenity enum

---
+++ src/main/java/com/roomsbooking/backend/enums/RoomAmenity.java
@@ -0,0 +1,18 @@
+package com.roomsbooking.backend.enums;
+
+/**
+ * Enums that represents amenity of a room.
+ */
+public enum RoomAmenity {
+    AIR_CONDITIONING,
+    KITCHENETTE,
+    KITCHEN,
+    BALCONY,
+    TV,
+    WASHER,
+    NETFLIX,
+    PRIVATE_BATHROOM,
+    FRIDGE,
+    MICROWAVE,
+    IRON
+}
