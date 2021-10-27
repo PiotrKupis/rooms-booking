0ead94110115de5c68e052cb2bc3a4751398d48d
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-27 16:14:34 +0200

    add image repository

---
+++ src/main/java/com/roomsbooking/backend/repository/ImageRepository.java
@@ -0,0 +1,13 @@
+package com.roomsbooking.backend.repository;
+
+import com.roomsbooking.backend.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository responsible for managing object of type {@link Image}.
+ */
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
